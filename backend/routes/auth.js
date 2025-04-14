d6f7a53af19a14078c069a7f2e5941cc2dd4dfee
github_student <jeremypoliquit.student@gmail.com>
2025-04-15 00:57:05 +0800

    router for api full and half

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,23 @@
-const express = require('express');
-const { register, login, profile } = require('../controllers/authController');
-const verifyToken = require('../middleware/auth');
+const express = require("express");
+const {
+  register,
+  login,
+  loginAdmin,
+  profile,
+  profileAdmin,
+  createAdmin,
+} = require("../controllers/authController");
+const verifyToken = require("../middleware/auth");
 
 module.exports = (db) => {
   const router = express.Router();
-  router.post('/register', (req, res) => register(req, res, db));
-  router.post('/login', (req, res) => login(req, res, db));
-  router.get('/profile', verifyToken, (req, res) => profile(req, res, db));
+  router.post("/register", (req, res) => register(req, res, db));
+  router.post("/login", (req, res) => login(req, res, db)); // student login
+  router.post("/create/admin", (req, res) => createAdmin(req, res, db)); // create admin/professor
+  router.post("/login/admin", (req, res) => loginAdmin(req, res, db)); // login admin/prof
+  router.get("/profile", verifyToken, (req, res) => profile(req, res, db)); // record of student
+  router.get("/profile/admin", verifyToken, (req, res) =>
+    profileAdmin(req, res, db)
+  );
   return router;
 };
--- backend/routes/crud.js
+++ backend/routes/crud.js
@@ -1,9 +1,13 @@
 const express = require('express');
-const { student, records } = require('../controllers/authController');
+const { createStudent, records, createStudentRecord, createStudentAndRecord, addRecordStudent } = require('../controllers/authController');
 
 module.exports = (db) => {
   const router = express.Router();
-  router.post('/create', (req, res) => student(req, res, db));
-  router.post('/create/record', (req, res) => records(req, res, db));
+  // router.post('/create/info', (req, res) => createStudentRecord(req, res, db));
+  // router.post('/create/record', (req, res) => addRecordStudent(req, res, db));
+  router.post('/create/record', (req, res) => createStudentRecord(req, res, db));
+  router.post('/add-record-only', (req, res) => addRecordStudent(req, res, db));
+  // router.post('/create/postman', (req, res) => createStudent(req, res, db));
+  // router.post('/create/record/postman', (req, res) => records(req, res, db));
   return router;
 };
