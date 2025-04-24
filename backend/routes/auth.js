8cb4060ef1b9b711bf6f346018d049da24449598
github_student <jeremypoliquit.student@gmail.com>
2025-04-24 18:34:28 +0800

    logic for admins

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,9 +1,6 @@
 const express = require("express");
 const {
-  register,
-  login,
   loginAdmin,
-  profile,
   profileAdmin,
   createAdmin,
 } = require("../controllers/authController");
@@ -11,11 +8,8 @@ const verifyToken = require("../middleware/auth");
 
 module.exports = (db) => {
   const router = express.Router();
-  router.post("/register", (req, res) => register(req, res, db));
-  router.post("/login", (req, res) => login(req, res, db)); // student login
   router.post("/create/admin", (req, res) => createAdmin(req, res, db)); // create admin/professor
   router.post("/login/admin", (req, res) => loginAdmin(req, res, db)); // login admin/prof
-  router.get("/profile", verifyToken, (req, res) => profile(req, res, db)); // record of student
   router.get("/profile/admin", verifyToken, (req, res) =>
     profileAdmin(req, res, db)
   );
